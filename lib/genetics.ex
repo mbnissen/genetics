7081c70bbe93d89696a0977be7faf86337d06ffb
Morten Berg Nissen <mbn@coinify.com>
2021-01-10 10:37:18 +0100

    Chapter 3 - solving OneMax using behaviours

--- lib/genetics.ex
+++ lib/genetics.ex
@@ -3,6 +3,8 @@ defmodule Genetics do
   Basic parts of the genetic algorithm
   """
 
+  alias Types.Chromosome
+
   def initialize(genotype, opts \\ []) do
     population_size = Keyword.get(opts, :population_size, 100)
     for _ <- 1..population_size, do: genotype.()
@@ -10,6 +12,11 @@ defmodule Genetics do
 
   def evaluate(population, fitness_function, opts \\ []) do
     population
+    |> Enum.map(fn chromosome ->
+      fitness = fitness_function.(chromosome)
+      age = chromosome.age + 1
+      %Chromosome{chromosome | fitness: fitness, age: age}
+    end)
     |> Enum.sort_by(fitness_function, &>=/2)
   end
 
@@ -21,9 +28,10 @@ defmodule Genetics do
 
   def crossover(population, opts \\ []) do
     Enum.reduce(population, [], fn {p1, p2}, acc ->
-      cx_point = :rand.uniform(length(p1))
-      {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
-      [h1 ++ t2 | [h2 ++ t1 | acc]]
+      cx_point = :rand.uniform(length(p1.genes))
+      {{h1, t1}, {h2, t2}} = {Enum.split(p1.genes, cx_point), Enum.split(p2.genes, cx_point)}
+      {c1, c2} = {%Chromosome{genes: h1 ++ t2}, %Chromosome{genes: h2 ++ t1}}
+      [c1 | [c2 | acc]]
     end)
   end
 
@@ -31,33 +39,33 @@ defmodule Genetics do
     population
     |> Enum.map(fn chromosome ->
       if :rand.uniform() < 0.05 do
-        Enum.shuffle(chromosome)
+        %Chromosome{genes: Enum.shuffle(chromosome.genes)}
       else
         chromosome
       end
     end)
   end
 
-  def run(fitness_function, genotype, max_fitness, opts \\ []) do
-    population = initialize(genotype, opts)
+  def run(problem, opts \\ []) do
+    population = initialize(&problem.genotype/0, opts)
 
     population
-    |> evolve(fitness_function, genotype, max_fitness, opts)
+    |> evolve(problem, opts)
   end
 
-  def evolve(population, fitness_function, genotype, max_fitness, opts \\ []) do
-    population = evaluate(population, fitness_function)
+  def evolve(population, problem, opts \\ []) do
+    population = evaluate(population, &problem.fitness_function/1, opts)
     best = hd(population)
-    IO.write("\rCurrency Best: #{fitness_function.(best)}")
+    IO.write("\rCurrency Best: #{best.fitness}")
 
-    if fitness_function.(best) == max_fitness do
+    if problem.terminate?(population) do
       best
     else
       population
       |> select(opts)
       |> crossover(opts)
       |> mutation(opts)
-      |> evolve(fitness_function, genotype, max_fitness, opts)
+      |> evolve(problem, opts)
     end
   end
 end
--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -1,10 +1,22 @@
-genotype = fn -> for _ <-1..42, do: Enum.random(0..1) end
+defmodule OneMax do
+  @behaviour Problem
 
-fitness_function = fn chromosome -> Enum.sum(chromosome) end
+  alias Types.Chromosome
 
-max_fitness = 42
+  @impl true
+  def genotype do
+    genes = for _ <- 1..42, do: Enum.random(0..1)
+    %Chromosome{genes: genes, size: 42}
+  end
 
-solution = Genetics.run(fitness_function, genotype, max_fitness)
+  @impl true
+  def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
+
+  @impl true
+  def terminate?(population), do: hd(population).fitness == 42
+end
+
+solution = Genetics.run(OneMax)
 
 IO.write("\n")
 IO.inspect(solution)
