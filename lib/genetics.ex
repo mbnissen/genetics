9e4b0782667594d4c25577b51062dd64ee05e657
Morten Berg Nissen <mbn@coinify.com>
2021-01-10 09:54:46 +0100

    Chapter 2 - use generic approach

--- lib/genetics.ex
+++ lib/genetics.ex
@@ -1,18 +1,63 @@
 defmodule Genetics do
   @moduledoc """
-  Documentation for `Genetics`.
+  Basic parts of the genetic algorithm
   """
 
-  @doc """
-  Hello world.
+  def initialize(genotype, opts \\ []) do
+    population_size = Keyword.get(opts, :population_size, 100)
+    for _ <- 1..population_size, do: genotype.()
+  end
 
-  ## Examples
+  def evaluate(population, fitness_function, opts \\ []) do
+    population
+    |> Enum.sort_by(fitness_function, &>=/2)
+  end
 
-      iex> Genetics.hello()
-      :world
+  def select(population, opts \\ []) do
+    population
+    |> Enum.chunk_every(2)
+    |> Enum.map(&List.to_tuple(&1))
+  end
 
-  """
-  def hello do
-    :world
+  def crossover(population, opts \\ []) do
+    Enum.reduce(population, [], fn {p1, p2}, acc ->
+      cx_point = :rand.uniform(length(p1))
+      {{h1, t1}, {h2, t2}} = {Enum.split(p1, cx_point), Enum.split(p2, cx_point)}
+      [h1 ++ t2 | [h2 ++ t1 | acc]]
+    end)
+  end
+
+  def mutation(population, opts \\ []) do
+    population
+    |> Enum.map(fn chromosome ->
+      if :rand.uniform() < 0.05 do
+        Enum.shuffle(chromosome)
+      else
+        chromosome
+      end
+    end)
+  end
+
+  def run(fitness_function, genotype, max_fitness, opts \\ []) do
+    population = initialize(genotype, opts)
+
+    population
+    |> evolve(fitness_function, genotype, max_fitness, opts)
+  end
+
+  def evolve(population, fitness_function, genotype, max_fitness, opts \\ []) do
+    population = evaluate(population, fitness_function)
+    best = hd(population)
+    IO.write("\rCurrency Best: #{fitness_function.(best)}")
+
+    if fitness_function.(best) == max_fitness do
+      best
+    else
+      population
+      |> select(opts)
+      |> crossover(opts)
+      |> mutation(opts)
+      |> evolve(fitness_function, genotype, max_fitness, opts)
+    end
   end
 end
